221c04c5e263ee219838f966bfb19dcdaa43ef70
Pomke Nohkan <pomke@pomke.com>
2014-08-04 17:07:03 +1000

    working code, needs more tests

--- index.js
+++ index.js
@@ -1,8 +1,105 @@
+var safe = {safe: true, journal: true, w : 1};
 
-exports.start = function(collection, match, worker, pollTime, lockTime) {
-    if(!collection || !match || |!worker) throw new Error("missing argument");
-    if(!pollTime) pollTime = 5000; //Poll every 5 seconds.
+exports.queue = function(collection, task, callback) {
+    if(!callback) callback = function(){};
+    collection.insert(task, safe, callback);
+};
+
+exports.start = function(collection, match, worker, pollTime, lockTime, log) {
+    if(!collection || !match || !worker) throw new Error("missing argument");
+    if(!pollTime) pollTime = 1000; //Poll every 1 second.
     if(!lockTime) lockTime = 900000; //Lock lasts 15 minutes.
+    if(!log) log = console.log;
 
+    //processing closure
+    function processTasks(callback) {
+        // find tasks that are not currently locked
+        var now = Date.now();
+        var query = {
+            $and : [
+                {$or : [
+                    {'attrition.locked' : null},
+                    {'attrition.locked' : {$lt : now-lockTime}}]},
+                    {'attrition.blocked' : null}
+            ]
+        };
+        if(match) query.$and.push(match);
+        function find() {
+            // lock a task for processing.
+            collection.findAndModify(
+                query, {}, {$set: {'attrition.locked': now}}, safe,
+                function (err, task) {
+                    // check we didn't get an error from the query.
+                    if (err) { log(msg); return callback(new Error(msg)); }
+
+                    // Handle getting no results (empty queue).
+                    if (!task)  return callback(null);
+
+                    // Run the worker function.
+                    try { worker(task, done); } catch (err) { done(err); }
+
+                    function done(err, keepTask, updates) {
+
+                        if (err) {
+                            log("error processing task: ",
+                                JSON.stringify(task, null, 2),
+                                err.stack);
+                            // mark the task as blocked.
+                            keepTask = true;
+                            updates = {
+                                $set : {
+                                    'attrition.blocked' : true,
+                                    'attrition.firstErrored' : now,
+                                    'attrition.error': err.stack||err.toString()
+                                },
+                                $inc : {'attrition.errorCount' : 1}
+                            };
+                        }
+                        if (keepTask) {
+                            // completed: unlock and apply optional updates.
+                            if(!updates) updates = {};
+                            if(!updates.$set) updates.$set = [];
+                            updates.$set.locked = null; // unlock the task.
+                            collection.update({_id : task._id}, updates, safe,
+                                    function (err) {
+                                        if (err) {
+                                            log("Error unlocking task:",
+                                                JSON.stringify(task,null,2),
+                                                "with update",
+                                                JSON.stringify(updates,null,2),
+                                                err.toString());
+                                        }
+                                        // find the next task.
+                                        process.nextTick(find);
+                                    });
+                        } else {
+                            // completed: remove task from queue.
+                            collection.remove({_id : task._id}, safe,
+                                    function (err) {
+                                        if (err) {
+                                            log("error removing task:",
+                                                JSON.stringify(task,null,2),
+                                                err.toString());
+                                        }
+                                        // find the next task.
+                                        process.nextTick(find);
+                                    });
+                        }
+                    }
+                });
+        }
+        find();
+
+    }
+
+    //Start polling queue
+    var running = true;
+    (function poll(err) {
+        if(err) console.log('queue error', err);
+        setTimeout(function() {if(running){processTasks(poll);}}, pollTime);
+    })();
+
+    //Return a function that can be used to stop the poll
+    return function stop() { running = false; };
 };
 
--- test/test_attrition.js
+++ test/test_attrition.js
@@ -1,11 +1,36 @@
+var assert    = require('assert');
+var mongodb   = require('mongodb');
+var attrition = require('../index');
+
+describe("Attrition", function() {
+
+    var db, queue;
+
+    before(function(done){
+        mongodb.MongoClient.connect('mongodb://localhost:27017/test_attrition',
+            function(err, database) {
+                if(err) throw err;
+                db = database;
+                queue = db.collection('test-queue');
+                done();
+            });
+    });
+
+    after(function(done){
+            db.dropDatabase(done);
+    });
+
+    it("should process a task", function(done) {
+
+        var stop = attrition.start(queue, {}, function(task, callback) {
+            assert.equal(task.payload, 123);
+            callback(null, false);
+            done();
+        });
+
+        attrition.queue(queue, {payload : 123});
+    });
 
 
-var attrition = require("attrition");
-
-var stopWorkers = attrition.start(col, filter, worker, pollTime, lockTime);
-
-attrition.send(col, task, callback);
-
-attrition.healthCheck(col, callback);
-
+});
 
